edbe5b23e710dddb55e3e31513426f5e7b946371
Marco Cárcamo <81669610+marcocarcamo-tech@users.noreply.github.com>
2022-04-08 17:02:52 -0500

    change path

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,5 @@ module.exports = defineConfig({
   transpileDependencies: true,
 });
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/https://marcocarcamo-tech.github.io/portfolio-vuejs//" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "https://marcocarcamo-tech.github.io/portfolio-vuejs//" : "/",
 };
