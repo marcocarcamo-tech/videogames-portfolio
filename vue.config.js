de9d049fb833795e65b729acea8c062d3795766a
Marco <macdev.gmail@gmail.com>
2022-04-08 17:23:26 -0500

    changes to path

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,3 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  transpileDependencies: true,
-});
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "https://marcocarcamo-tech.github.io/portfolio-vuejs/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/portfolio-vuejs/" : "/",
 };
